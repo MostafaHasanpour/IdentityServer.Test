8a3a8aa90309e217913d5382c1e55ea28601adde
Mostafa Hasanpour <developer.hasanpour@outlook.com>
2019-05-26 07:38:00 +0430

    add web app for test identity server

--- IdentityServer.Web/Program.cs
+++ IdentityServer.Web/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace IdentityServer.Api
+namespace IdentityServer.Web
 {
     public class Program
     {
