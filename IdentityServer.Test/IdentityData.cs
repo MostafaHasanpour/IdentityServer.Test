3a61065348c5f3962b9b79bdd5980a522e394428
Mostafa Hasanpour <developer.hasanpour@outlook.com>
2019-05-26 05:52:39 +0430

    move users & resource & clients to identity data class

---
+++ IdentityServer.Test/IdentityData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityModel;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace IdentityServer.Test
+{
+    public class IdentityData
+    {
+        public static IEnumerable<ApiResource> GetResources()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("api1","api1 Display Name")
+            };
+        }
+
+        internal static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>()
+            {
+                new Client()
+                {
+                    ClientId="Postman",
+                    ClientSecrets=new []{new Secret("secret".ToSha256())}
+                }
+            };
+        }
+
+        internal static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>()
+            {
+                new TestUser()
+                {
+                    Password="P@$$w0rd",
+                    Username="user",
+                    SubjectId = "1",
+                }
+            };
+        }
+    }
+}
--- IdentityServer.Test/Startup.cs
+++ IdentityServer.Test/Startup.cs
@@ -27,12 +27,12 @@ namespace IdentityServer.Test
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddIdentityServer().AddDeveloperSigningCredential().
-                AddTestUsers(new List<IdentityServer4.Test.TestUser>()
+                AddTestUsers(IdentityData.GetUsers()>()
                 {
                     new IdentityServer4.Test.TestUser(){SubjectId="64d83ae0-57f2-4ceb-843f-30cf30b50fd1", Username="user", Password= "P@$$w0rd"}
                 }).
-                AddInMemoryApiResources(new List<ApiResource>()).
-                AddInMemoryClients(new List<Client>());//then go to this url https://localhost:44390/.well-known/openid-configuration
+                AddInMemoryApiResources(IdentityData.GetResources()).
+                AddInMemoryClients(IdentityData.GetClients());//then go to this url https://localhost:44390/.well-known/openid-configuration
 
 
 
