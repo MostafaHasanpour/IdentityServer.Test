39ab7af3d971bc9455b5a828a46a4f44065ab78d
Mostafa Hasanpour <developer.hasanpour@outlook.com>
2019-05-26 08:49:23 +0430

    save resource, client and ... to db

--- IdentityServer.Test/Startup.cs
+++ IdentityServer.Test/Startup.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using IdentityServer4.Models;
+using IdentityServer4.Validation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,12 +31,27 @@ namespace IdentityServer.Test
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            const string connectionString = @"Data Source=.;database=IdentityServer4Db;trusted_connection=yes;";
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
             services.AddIdentityServer().AddDeveloperSigningCredential().
                 AddTestUsers(IdentityData.GetUsers()).
-                AddInMemoryApiResources(IdentityData.GetResources()).
-                AddInMemoryIdentityResources(IdentityData.GetIdentityResources()).
-                AddInMemoryClients(IdentityData.GetClients());//then go to this url https://localhost:44390/.well-known/openid-configuration
-
+                //AddInMemoryApiResources(IdentityData.GetResources()).
+                //AddInMemoryIdentityResources(IdentityData.GetIdentityResources()).
+                //AddInMemoryClients(IdentityData.GetClients());//then go to this url https://localhost:44390/.well-known/openid-configuration
+                AddConfigurationStore(o =>
+                {
+                    o.ConfigureDbContext = builder =>
+                      builder.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(o =>
+                {
+                    o.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+                    o.EnableTokenCleanup = true;
+                    o.TokenCleanupInterval = 30;
+                })
+                .AddResourceOwnerValidator<ResourceOwnerValidator>();
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
@@ -40,6 +60,7 @@ namespace IdentityServer.Test
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            InitializeDatabase(app);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -53,6 +74,51 @@ namespace IdentityServer.Test
             app.UseIdentityServer();
             app.UseHttpsRedirection();
             app.UseMvcWithDefaultRoute();
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in IdentityData.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in IdentityData.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in IdentityData.GetResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+
+    public class ResourceOwnerValidator : IResourceOwnerPasswordValidator
+    {
+        public Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            throw new NotImplementedException();
         }
     }
 }
